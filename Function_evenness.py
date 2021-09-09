9d3411ea8cae1d30ed5a61ab7b4186721d3f7439
ms1969 <maxmaxm@mail.ru>
2021-09-09 13:51:30 +0800

    Function lerning

---
+++ Function_evenness.py
@@ -0,0 +1,29 @@
+def odd_even (number):
+    if int(number) % 2 == 0:
+        return True
+    else:
+        return False
+number = input("Введите число: ")
+result = odd_even (number)
+if result == True:
+    print("четное")
+else:
+    print("нечетное")
+
+5
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
