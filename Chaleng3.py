80d5c8efdb41bd49e411ea8631b5c9c62ac39e1e
ms1969 <maxmaxm@mail.ru>
2021-09-20 21:12:29 +0800

    change_question

--- Chaleng3.py
+++ Chaleng3.py
@@ -1,28 +1,22 @@
 
-def likes (names):
-    message= ["no one likes this","Peter likes this","Jacob and Alex like this","Max, John and Mark like this","Alex, Jacob and 2 others like this"]
-    if names == []:
-        return(message[0])
-    elif names == ["Peter"]:
-        return(message[1])
-    elif names == ["Jacob", "Alex"]:
-        return(message[2])
-    elif names == ["Max", "John", "Mark"]:
-        return(message[3])
-    elif len(names) > 3:
-        return "Alex, Jacob and " + str(len(names)-2) +  " others like this"
+def descending_order(num):
+    nList = []
+    sNum = str(num)
+    for item in range(0, len(str(num))):
+        nList.append(sNum[item])
+    nList.sort()
+    sNum = ''
+    for item in range(0, len(str(num))):
+        sNum = sNum + str((nList.pop()))
+    return int(sNum)
+
+print (descending_order(13409578110))
+
+
     
-    
-print (likes(["Alex", "Jacob", "Mark", "Max", "James"])) 
 
 
 
-#for letter in 'Python':
-#    if letter == 'y':   
-#        print('This is pass block')
-#    print(letter)
-#import sys
-#print(sys.version)
 
 
 
