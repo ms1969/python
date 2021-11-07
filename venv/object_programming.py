f4bf85c0415ae07758e4f9cd4a51f08112cb5ec9
ms1969 <maxmaxm@mail.ru>
2021-11-07 13:01:42 +0800

    stacker

--- venv/object_programming.py
+++ venv/object_programming.py
@@ -119,20 +119,20 @@
 # print(p1.full_name())  # выводит "Hendrix Jimi"
 # print(p1.is_adult()) # выводит "True"
 
-class Dog:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def description(self):
-        return (f'{self.name} is {self.age} years old')
-
-    def speak(self, say):
-        return (f'{self.name} says {say}')
-
-
-jack = Dog("Jack", 4)
-
-print(jack.description())  # распечатает 'Jack is 4 years old'
-print(jack.speak("Woof Woof"))  # распечатает 'Jack says Woof Woof'
-print(jack.speak("Bow Wow"))  # распечатает 'Jack says Bow Wow'
+# class Dog:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def description(self):
+#         return (f'{self.name} is {self.age} years old')
+#
+#     def speak(self, say):
+#         return (f'{self.name} says {say}')
+#
+#
+# jack = Dog("Jack", 4)
+#
+# print(jack.description())  # распечатает 'Jack is 4 years old'
+# print(jack.speak("Woof Woof"))  # распечатает 'Jack says Woof Woof'
+# print(jack.speak("Bow Wow"))  # распечатает 'Jack says Bow Wow'
