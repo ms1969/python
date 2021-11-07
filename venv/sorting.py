5f3b7298c7d430018a7da14ac8a42fb814f56197
ms1969 <maxmaxm@mail.ru>
2021-11-07 11:28:22 +0800

    Merge branch 'master' of https://github.com/ms1969/python

--- venv/sorting.py
+++ venv/sorting.py
@@ -21,18 +21,20 @@
 # for i in subject_marks:
 #     print(i[0], i[1])
 
-models = [{'make': 'Nokia', 'model': 216, 'color': 'Black'},
-          {'make': 'Mi Max', 'model': 2, 'color': 'Gold'},
-          {'make': 'Samsung', 'model': 7, 'color': 'Blue'},
-          {'make': 'Apple', 'model': 10, 'color': 'Silver'},
-          {'make': 'Oppo', 'model': 9, 'color': 'Red'},
-          {'make': 'Huawei', 'model': 4, 'color': 'Grey'},
-          {'make': 'Honor', 'model': 3, 'color': 'Black'}]
-models = sorted(models, key=lambda x: x['color'])
-for i in models:
-    print(f"Производитель: {i['make']}, модель: {i['model']}, цвет: {i['color']}")
+# models = [{'make': 'Nokia', 'model': 216, 'color': 'Black'},
+#           {'make': 'Mi Max', 'model': 2, 'color': 'Gold'},
+#           {'make': 'Samsung', 'model': 7, 'color': 'Blue'},
+#           {'make': 'Apple', 'model': 10, 'color': 'Silver'},
+#           {'make': 'Oppo', 'model': 9, 'color': 'Red'},
+#           {'make': 'Huawei', 'model': 4, 'color': 'Grey'},
+#           {'make': 'Honor', 'model': 3, 'color': 'Black'}]
+# models = sorted(models, key=lambda x: x['color'])
+# for i in models:
+#     print(f"Производитель: {i['make']}, модель: {i['model']}, цвет: {i['color']}")
 
-
+a = 100
+condition = a % 2 == 0
+print(condition)
 
 
 
