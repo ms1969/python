f340e06787842ef069ab07dece56b4690e224c60
ms1969 <maxmaxm@mail.ru>
2022-01-27 22:33:00 +0800

    Задача Иосифа Флавия

--- myprog-7.py
+++ myprog-7.py
@@ -3,18 +3,28 @@
 # счету человек выбывает из круга, после чего счет продолжается со следующего за ним человека.
 # Напишите программу, определяющую номер человека, который останется в кругу последним.
 
+
+# Мое решение
+
 n = int(input())
 k = int(input())
 m = [i for i in range(1, n + 1)]
-lposition = 0
+lposition = 1
 while len(m) > 1:
-    for i in range(1, k+1):
-        if lposition >= len(m):
-            lposition = 0
+    for i in range(1, k + 1):
+        if i == k:
+            m.remove(m[lposition - 1])
         else:
             lposition += 1
-        if i == k:
-            m.remove(m[lposition-1])
+        if lposition > len(m):
+            lposition = 1
 print(m[0])
 
+# Лучшее решение
 
+# n = int(input())
+# k = int(input())
+# res = 0
+# for i in range(1, n+1):
+#     res = (res + k) % i
+# print(res + 1)
